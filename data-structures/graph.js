32ab086cca76b65733184988ad3c62acc69ac32b
Fajar Hidayad <fajarsuryahidayad@gmail.com>
2024-06-18 08:49:04 +0700

    completed graph

--- data-structures/graph.js
+++ data-structures/graph.js
@@ -11,15 +11,46 @@ class Graph {
     return false;
   }
 
-  addEdge() {}
+  addEdge(vertex1, vertex2) {
+    if (!this.adjacencyList[vertex1] || !this.adjacencyList[vertex2])
+      return false;
 
-  removeEdge() {}
+    if (this.adjacencyList[vertex1].find((v) => v === vertex2)) return false;
 
-  removeVertex() {}
+    this.adjacencyList[vertex1].push(vertex2);
+    this.adjacencyList[vertex2].push(vertex1);
+    return true;
+  }
+
+  removeEdge(vertex1, vertex2) {
+    if (!this.adjacencyList[vertex1] || !this.adjacencyList[vertex2])
+      return false;
+
+    this.adjacencyList[vertex1] = this.adjacencyList[vertex1].filter(
+      (v) => v !== vertex2
+    );
+    this.adjacencyList[vertex2] = this.adjacencyList[vertex2].filter(
+      (v) => v !== vertex1
+    );
+    return true;
+  }
+
+  removeVertex(vertex) {
+    if (!this.adjacencyList[vertex]) return undefined;
+    // let edges = this.adjacencyList[vertex];
+
+    // for (let i = 0; i < edges.length; i++) {
+    //   this.removeEdge(vertex, edges[i]);
+    // }
+
+    while (this.adjacencyList[vertex].length) {
+      let temp = this.adjacencyList[vertex].pop();
+      this.removeEdge(vertex, temp);
+    }
+
+    delete this.adjacencyList[vertex];
+    return this;
+  }
 }
 
 const graph = new Graph();
-graph.addVertex('A');
-graph.addVertex('B');
-graph.addVertex('A');
-console.log(graph);
