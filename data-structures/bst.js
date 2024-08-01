6a72595d3c934fd22da71ea2d7ba3318cad42165
Fajar Hidayad <fajarsuryahidayad@gmail.com>
2024-08-02 05:32:44 +0700

    added bst:recursive insert & delete node

--- data-structures/bst.js
+++ data-structures/bst.js
@@ -57,6 +57,66 @@ class BST {
 
     return false;
   }
+
+  rContains(value, currentNode = this.root) {
+    if (currentNode === null) return false;
+
+    if (currentNode.value === value) return true;
+
+    if (value < currentNode.value)
+      return this.rContains(value, currentNode.left);
+    else return this.rContains(value, currentNode.right);
+  }
+
+  #rInsert(value, currentNode = this.root) {
+    if (currentNode === null) return new Node(value);
+
+    if (value < currentNode.value)
+      currentNode.left = this.#rInsert(value, currentNode.left);
+    else if (value > currentNode.value)
+      currentNode.right = this.#rInsert(value, currentNode.right);
+
+    return currentNode;
+  }
+
+  rInsert(value) {
+    if (this.root === null) this.root = new Node(value);
+    return this.#rInsert(value);
+  }
+
+  minValue(currentNode) {
+    while (currentNode.left !== null) {
+      currentNode = currentNode.left;
+    }
+    return currentNode.value;
+  }
+
+  #deleteNode(value, currentNode) {
+    if (currentNode === null) return null;
+
+    if (value < currentNode.value)
+      currentNode.left = this.#deleteNode(value, currentNode.left);
+    else if (value > currentNode.value)
+      currentNode.right = this.#deleteNode(value, currentNode.right);
+    else {
+      if (currentNode.left === null && currentNode.right === null) {
+        return null;
+      } else if (currentNode.left === null) {
+        currentNode = currentNode.right;
+      } else if (currentNode.right === null) {
+        currentNode = currentNode.left;
+      } else {
+        let subTreeMin = this.minValue(currentNode.right);
+        currentNode.value = subTreeMin;
+        currentNode.right = this.#deleteNode(subTreeMin, currentNode.right);
+      }
+    }
+    return currentNode;
+  }
+
+  deleteNode(value) {
+    this.root = this.#deleteNode(value, this.root);
+  }
 }
 
-const tree = new BST();
+const bst = new BST();
